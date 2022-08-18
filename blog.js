4c94b86c018de3727e217382083abff786641971
sayedjilliyan <jilliyan67@gmail.com>
2022-08-19 04:03:27 +0700

    perbaikan icon

--- blog.js
+++ blog.js
@@ -141,4 +141,71 @@ function renderBlog(){
 
 }
 
-let data
+
+function getFullTime(time){
+
+    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    // console.log(date);
+    // console.log(month[monthIndex]);
+    // console.log(year);
+
+    // console.log(hours);
+    // console.log(minutes);
+
+    if(hours < 10){
+        hours = "0" + hours
+    }else if(minutes < 10){
+        minutes = "0" + minutes
+    }
+    
+    // 12 Agustus 2022 09.04
+    let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
+    // console.log(fullTime);
+    return fullTime
+}
+
+function getDistanceTime(time){
+
+    let timeNow = new Date()
+    let timePost = time
+
+    let distance = timeNow - timePost
+    console.log(distance);
+
+    let milisecond = 1000 // 1 detik 1000 milisecond
+    let secondInHours = 3600 // 1 jam sama dengan 3600 detik
+    let hoursInDay = 24 // 1 hari 24 jam
+
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+    let distanceMinutes = Math.floor(distance / (milisecond * 60))
+    let distanceSeconds = Math.floor(distance / milisecond)
+
+    if(distanceDay > 0){
+        return `${distanceDay} day ago`
+    } else if(distanceHours > 0){
+        return `${distanceHours} hours ago`
+    } else if(distanceMinutes > 0){
+        return `${distanceMinutes} minutes ago`
+    } else {
+        return `${distanceSeconds} seconds ago`
+    }
+}
+
+// function distanceDate(start, end){
+
+// }
+
+
+
+// setInterval(function(){
+//     renderBlog()
+// }, 3000)
